90ce14ba9fd8e2fbaf8cae26137d417933257ec9
Thien <huynhminhthien95@gmail.com>
2022-02-28 22:11:00 +0700

    Update page index

--- resources/views/frontside/page/index.blade.php
+++ resources/views/frontside/page/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <section class="main">
         @if($slug != "term-and-conditions")
+            @if (!blank($data->landscape_image))
         <div class="page-visual" style="background-image: url('{{ voyager::image($data->landscape_image) }}');"></div>
+            @endif
         @endif
         <div class="container post-detail {{ $slug == "term-and-conditions" ? 'padding-topnn' : ''}}">
             <h2 class="primary-title text-center">
