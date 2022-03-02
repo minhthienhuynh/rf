cd043562cba8600c9146ebc68850c757055e4207
minhnguyenwp <minhnguyenwp@gmail.com>
2022-03-02 14:09:29 +0700

    Update footer

--- resources/views/layouts/includes/menu-footer.blade.php
+++ resources/views/layouts/includes/menu-footer.blade.php
@@ -4,7 +4,7 @@
         $originalItem = $item;
     @endphp
     <ul class="f-nav">
-        <li class="f-nav-ttl"><a href="{{ url($item->link()) }}">{{$item->title}}</a></li>
+        <li class="f-nav-ttl">{{$item->title}}</li>
         @if(!$originalItem->children->isEmpty())
             @foreach ($originalItem->children as $child)
                 <li><a href="{{ url($child->link()) }}">{{ $child->title }}</a></li>
