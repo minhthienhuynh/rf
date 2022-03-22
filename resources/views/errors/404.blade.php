a9c0b2ed7538ba420543cd0756576f90b1f5f0f8
Thien <huynhminhthien95@gmail.com>
2022-03-22 22:09:36 +0700

    FB27 - 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+    <section class="main">
+        <div class="page-visual" style="background-image: url('/frontside/assets/img/images/visual-img-05.png');"></div>
+        <div class="container">
+            <div class="not-found-wrap">
+                <h1 class="not-found-title">404</h1>
+                <div class="txt">Are you lost in the forest?</div>
+                <div class="desc">The page you requested could not be found.</div><a class="btn-more" href="/">Go to Homepage</a>
+            </div>
+        </div>
+    </section>
+@endsection
